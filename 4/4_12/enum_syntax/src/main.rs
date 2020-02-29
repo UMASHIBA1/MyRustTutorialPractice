1d84603fce702e89314f79c7930101ff8ba876c4
UMASHIBA <umashiba1@gmail.com>
2020-02-29 18:11:27 +0900

    4-12更新

--- 4/4_12/enum_syntax/src/main.rs
+++ 4/4_12/enum_syntax/src/main.rs
@@ -1,10 +1,10 @@
 // Rustにはenum型というものがある。
 // 自分なりの解釈ではTypeScriptでいうところの type Message = Quit | ChangeColor | Move | Write;みたいな感じ
 enum Message {
-    Quit,
-    ChangeColor(i32, i32, i32),
-    Move { x: i32, y: i32 },
-    Write(String),
+    Quit,                       //データをも持たないヴァリアントの定義方法
+    ChangeColor(i32, i32, i32), // タプル構造体のような名前なしの構造体の定義方法
+    Move { x: i32, y: i32 },    // 名前あり構造体の定義方法
+    Write(String),              //データを一つもつ定義方法
 }
 
 fn enum_test() {
